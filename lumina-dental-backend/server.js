a78b2cd5d806f1236da80874e8869c71699680cb
rayanubhav <rayanubhav36@gmail.com>
2025-10-26 22:58:25 +0530

    cors frontend addition

--- lumina-dental-backend/server.js
+++ lumina-dental-backend/server.js
@@ -19,7 +19,7 @@ app.set("trust proxy", 1);
  // Handles preflight requests
 
 const corsOptions = {
-    origin: ['http://localhost:5173',"https://dentavista-dental-care.vercel.app","https://first-project-for-api-458415.web.app"], 
+    origin: ['http://localhost:5173',"https://dentavista-dental-care.vercel.app","https://first-project-for-api-458415.web.app","https://first-project-for-api-458415.firebaseapp.com"], 
     methods: ['GET', 'POST', 'OPTIONS'],
     credentials: true,
 };
